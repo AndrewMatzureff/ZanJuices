3ef9f595e5e2ae5372ce232c8a0dd6935570eb2c
AndrewMatzureff <andrewmatzureff@gmail.com>
2024-10-17 20:57:05 -0700

    Add release shortcut, 'release' as default task and .gitignore

--- build.bat
+++ build.bat
@@ -1,15 +1,32 @@
 @echo off
 
-set "task_name=%~1"
-set task_args=%*
-call set task_args=%%task_args:*%1=%%
-echo: && echo [BUILD] INITIATING TASK "%task_name%" WITH ARGUMENTS "%task_args%"...
-call .\tasks\%task_name%.bat . %task_args%
-
-if %ERRORLEVEL%==0 (
-    echo [BUILD] TASK "%task_name%" SUCCESSFUL!
+rem Check if a task name argument was passed; if not then default to "release".
+if "%1"=="" (
+    set "task_name=release"
 ) else (
-    echo [BUILD] TASK "%task_name%" FAILED! Task corresponding to "%task_name%.bat" failed with error level: %ERRORLEVEL%. Is there a valid script defined in ".\tasks\%task_name%.bat"?
+    set "task_name=%~1"
 )
 
-echo: && pause
+rem Isolate arguments to pass into the desired task script.
+set "task_args=%*"
+call set "task_args=%%task_args:*%1=%%"
+
+rem Initiate desired task script.
+echo: && echo [BUILD] INITIATING TASK "%task_name%" WITH ARGUMENTS "%task_args%"...
+call ".\tasks\%task_name%.bat" "." "%task_args%"
+
+rem Alert the caller to whether the task succeeded or failed.
+if %ERRORLEVEL%==0 (
+    call:alert TASK "%task_name%" SUCCESSFUL!
+) else (
+    call:alert TASK "%task_name%" FAILED! Task corresponding to "%task_name%.bat" failed with error level: %ERRORLEVEL%. Is there a valid script defined in ".\tasks\%task_name%.bat"?
+)
+
+echo:
+pause
+exit /b
+
+rem Alert the caller to some important build-related info (basically an alias for "echo" but with a build label prepended to the message).
+:alert
+echo [BUILD] %*
+exit /b
--- tasks/release.bat
+++ tasks/release.bat
@@ -8,41 +8,49 @@ if "%1"=="" (
 )
 
 rem Store the directory path passed as the first argument.
-set "base_dir=%~1"
-set "src_dir=%base_dir%\src"
+set "project_root=%~1"
+set "src_dir=%project_root%\src"
 
 rem Check if the provided directory exists.
-if not exist "%base_dir%" (
-    call :alert The directory "%base_dir%" does not exist.
+if not exist "%project_root%" (
+    call:alert The directory "%project_root%" does not exist.
     exit /b 1
 )
 
-call:alert Compiling archive(s) from "%base_dir%"...
+call:alert Compiling archive(s) from "%project_root%"...
 
 rem Path to archiving application executable.
-set archiver_exe="C:\Program Files\7-Zip\7z"
+set "archiver_exe=C:\Program Files\7-Zip\7z"
 
 call:alert Generating artifacts...
 
 rem Retrieve directory name of the provided path.
-for %%F in ("%base_dir%") do (
+for %%F in ("%project_root%") do (
     set "dir_name=%%~nxF"
 )
 
 rem Set package-common output path relative to the parent of the provided directory.
-set output=%base_dir%\..\release\%dir_name%
+set "output=%project_root%\..\release\%dir_name%"
 
 rem Check if the output directory exists.
 if not exist "%output%" (
     mkdir "%output%" && call:alert Created output directory, "%output%".
 )
 
+rem Create shortcut to release-artifacts from project root.
+call "%project_root%\create-folder-shortcut" "%output%" "%project_root%" "release"
+
+set div================================================================================
+
 rem For each subfolder in the src directory, compile a corresponding pk3.
 for /d %%G in ("%src_dir%\*") do (
-    %archiver_exe% a -tzip "%output%\%%~nxG.pk3" "%src_dir%\%%~nxG\*" && call:alert Finished compiling archive to %%~nxG.pk3!
+	call:alert [%%~nxG.pk3]
+	echo %div%
+    "%archiver_exe%" a -tzip "%output%\%%~nxG.pk3" "%src_dir%\%%~nxG\*" && echo: && echo %div% && call:alert Finished compiling archive to "%%~nxG.pk3"!
 )
 
 echo:
+exit /b
 
 rem Alert the caller to some important task-related info (basically an alias for "echo" but with a task label prepended to the message).
 :alert
